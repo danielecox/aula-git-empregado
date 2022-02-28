511e4c6f78c6959b98be4ceae3edbe4c11ca0148
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2022-02-28 10:10:56 -0300

    Incluindo arquivos faltantes do commit anterior

--- Empregado.java
+++ Empregado.java
@@ -1,35 +1,46 @@
-public class Empregado{
+public class Empregado {
     private String nome;
     private int idade;
     private int tipo;
     private double salario;
     private double comissao;
     private double bonus;
-    public double calculaSalario (){
-    if (tipo == 1){
-    return salario;
+
+    public double calculaSalario() {
+        if (tipo == 1) {
+            return salario;
+        } else if (tipo == 2) {
+            // comissão varia de 0 a 1, é um percentual sobre o salário
+            return salario + salario * comissao;
+        } else if (tipo == 3) {
+            return salario + bonus;
+        } else {
+            return 0.;
+        }
     }
-    else if (tipo == 2){
-    //comissão varia de 0 a 1, é um percentual sobre o salário
-    return salario + salario * comissao;
+
+    public String getNome() {
+        return this.nome;
     }
-    else if (tipo == 3){
-    return salario + bonus;
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
-    else{
-    return 0.;
+
+    public int getIdade() {
+        return this.idade;
     }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
-    public String getNome (){
-    return this.nome;
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
     }
-    public void setNome (String nome){
-    this.nome = nome;
+
+    public int tipo() {
+        return this.tipo;
     }
-    public int getIdade (){
-    return this.idade;
-    }
-    public void setIdade (int idade){
-    this.idade = idade;
-    }
-   }
+
+}
